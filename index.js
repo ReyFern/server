e4197aafdb1f4e4003e0edf0174cf25c015fc37e
Davian <rehan.davian@gmail.com>
2024-12-12 22:11:41 +0000

    Set up communication with client side and created gitignore

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 // Import express
 const express = require('express');
+const fsp = require("fs/promises");
+const cors = require("cors");
+
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]});
+app.use(cors());
+//app.use(bodyParser.urlencoded({extended: false}));
+//app.use(bodyParser.json());
+app.use(express.json());
+
+app.post("/create-character", async (req, res) => {
+    let character = req.body;
+    let characterFileHandle = await fsp.open("./json/character.json", "w");
+    characterFileHandle.write(JSON.stringify(character)).then(() => {
+        res.json({
+            state: "OK"
+        })
+    }).catch((e) => {
+        res.status(403).json({
+            state: "NOT OK"
+        });
+    });
+    characterFileHandle.close();
+
+});
+
+app.post("/save-character", async (req, res) => {
+    let character = req.body;
+    let characterFileHandle = await fsp.open("./json/character.json", "w");
+    characterFileHandle.write(JSON.stringify(character)).then(() => {
+        res.json({
+            state: "OK"
+        })
+    }).catch((e) => {
+        res.status(403).json({
+            state: "NOT OK"
+        });
+    });
+    characterFileHandle.close();
+
 });
 
 // Listens on port 5000
 app.listen(5000, () => {
-      console.log('server listening on port 5000');
+    console.log('server listening on port 5000');
 });
